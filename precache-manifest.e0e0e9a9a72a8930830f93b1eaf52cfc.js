0bcb912f9682f929ffd16e77ddf141b474a54df3
lirongyue <761780862@qq.com>
2020-06-01 23:55:04 +0800

    deploy

---
+++ precache-manifest.e0e0e9a9a72a8930830f93b1eaf52cfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e403c24f3e5955850c7286792e3ea76",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "002c563d99cb7aaa52ec",
+    "url": "/morney-react-website/static/css/2.02016362.chunk.css"
+  },
+  {
+    "revision": "31872279ea3cc59c9b17",
+    "url": "/morney-react-website/static/css/main.a8db464f.chunk.css"
+  },
+  {
+    "revision": "002c563d99cb7aaa52ec",
+    "url": "/morney-react-website/static/js/2.230db2ae.chunk.js"
+  },
+  {
+    "revision": "c9348b18d2bbe97efd15469c807b7dcd",
+    "url": "/morney-react-website/static/js/2.230db2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31872279ea3cc59c9b17",
+    "url": "/morney-react-website/static/js/main.0de9860f.chunk.js"
+  },
+  {
+    "revision": "1ca77655daa3211a8e8f",
+    "url": "/morney-react-website/static/js/runtime-main.136e4cb2.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/morney-react-website/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/morney-react-website/static/media/element-icons.d2f69a92.woff"
+  }
+]);
